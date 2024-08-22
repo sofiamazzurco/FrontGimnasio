269ac69357725ec1a2796882d4ac3d23dc98dd1c
Rodrimache <rodri.rs.93@gmail.com>
2024-08-21 21:34:40 -0300

    Botones de contacto

---
+++ FrontGimnasio/src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import Nav from 'react-bootstrap/Nav';
+import { FaFacebook, FaInstagram, FaMailBulk, FaWhatsapp} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <>
+      <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="https://www.facebook.com/">
+            <FaFacebook /> Facebook
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="https://www.instagram.com/">
+            <FaInstagram /> Instagram
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="mailto:gym@gmail.com">
+            <FaMailBulk /> Correo
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="https://wa.me/3412144421">
+            <FaWhatsapp /> WhatsApp
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
+}
+
+export default Footer;
